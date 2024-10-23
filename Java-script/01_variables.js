70dcd82350de05729a0abe294feed752b81529f6
Amit <amittiwari79099@gmail.com>
2024-10-23 20:08:28 +0530

    first commit

---
+++ Java-script/01_variables.js
@@ -0,0 +1,14 @@
+   const account_Id = 15520
+   let Email= "amittiwari79099@gmail.com"
+   var accountPassword = "1234"
+   accountCity = "jaipur"
+
+
+// console.log(account_Id)
+// console.log(Email);
+   account_Id = 520
+   accountCity = "Bhopal"
+   accountPassword = "4321"
+   let accout_State;
+   console.table([account_Id,accountCity,accountPassword,accout_State])
+   
---
+++ Java-script/02_datatype.js
@@ -0,0 +1,64 @@
+console.log("AMIT IS A GOOD BOY");
+// alert("Not found");
+/*console.log(typeof "amit");
+console.log(typeof age);1
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(2+2);
+console.log(2/2);
+console.log(2*2);
+let str1 = "hello"
+let str2 = "Amit"
+let str3 = str1 + str2
+console.log(str3)
+let a = 5
+a = a+2
+console.log(a);*/
+// jadu dekho milkar ke 
+let x= "amit tiwari"
+let y = 77;
+let z = 5.223;
+const w = false;
+let t = null;
+let l = undefined;
+
+console.log(x, y, z, w, t ,l );
+console.log(typeof x, typeof y, typeof z,typeof  w, typeof t, typeof l );
+//object has key and values known as object 
+//here, girl is just like oject 
+let girl= {
+    Name: "Prerna",
+    Age: "19 years",
+    Eyes: "black",
+}
+console.log(girl);
+//Now we are going to learn conditional statement in java script
+let age=18
+if (age >= 18) {
+    console.log("you can drive");
+} else {
+    console.log("you can not drive");
+}
+//now it's time to learn exponentiol operators 
+let r = 2
+let s = 3
+
+console.log(r ** s)
+//here the answer is 2 to the power 3 which is equal to 8
+let mohit = 23
+let year = 2
+
+console.log( "AGE OF MOHIT BHAI AFTER 2 YEARS = ",mohit += year)
+// now it's time to learn comparision operator
+const score = 100
+const scorevalue = 100.3
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+//refrence non-primtive 
+// Array, Objects, Functions
