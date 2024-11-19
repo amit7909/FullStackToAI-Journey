7388628366aba7f1a5cc12437db59249e85faed9
Amit <amittiwari79099@gmail.com>
2024-11-19 18:12:15 +0530

    Here i learn JavaScript Variable & Data Type

--- Java-script/Intro.js
+++ Java-script/Intro.js
@@ -1,1 +1,32 @@
+// Here we have to learn Intro,Data-type,variables
+//storage;CRUD 
 console.log('shikha is not my friend');
+//data type in JS
+/*
+String: Text form => ``,'',""
+Number: Numerical value
+Boolean: true,False
+BigInt: big integer value
+Symbol:Guarented unique value
+Undefined: Reserved but not define 
+Null:not reserved and not define,Empty;
+
+*/
+const a ='shikha';
+console.log(`Hello ${a}`);
+console.log(typeof a);//inside the string i am calling a variable
+const b = Symbol('Hello Tiwari ji')
+const c = Symbol('Hello Tiwari ji')
+console.log(b == c);//Ans is False 
+console.log(b != c);//Ans is True 
+
+// Symbols return True only when the compaired variable values are differ.
+
+//Let's go and see the magic 
+let m =0/0; 
+console.log(m);//this is a illegal operation show it return NaN.
+console.log(typeof m );//type of NaN is number
+ 
+
+
+
