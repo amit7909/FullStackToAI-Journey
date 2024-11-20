34bffa1abed339d346ec5d0f431429ef9711e8d9
Amit <amittiwari79099@gmail.com>
2024-11-20 18:37:22 +0530

    Day1 To learn operators

---
+++ Java-script/Operator.js
@@ -0,0 +1,10 @@
+/*Now make your learning Intresting with Java-Script*/
+const x = 20;
+const y = 30;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);//Exponential Operator
