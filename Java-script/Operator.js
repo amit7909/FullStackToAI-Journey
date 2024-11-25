95294d3c2c8bc6d592de0ff642652e3674a32238
Amit <amittiwari79099@gmail.com>
2024-11-25 21:02:45 +0530

    Learning Type-Conversion with practical  learning

--- Java-script/Operator.js
+++ Java-script/Operator.js
@@ -10,3 +10,25 @@ console.log(x % y);
 console.log(x ** y);//Exponential Operator
 
  //Here i am doing some problem solving
+ let num = 5;
+ num = num+5;
+ console.log(num);
+
+ let cart = 1000;
+ cart += 200;
+ console.log(cart);
+
+ /*comparission operators gives result in True or False 
+ 
+==: Equal operator :- will check only value 
+===: Strictly equal Operator:- will check both value and type of data  
+
+Logical Operator 
+
+AND :-it will return true only when all conditions are true &&
+
+OR :-it will return TRUE when any one of them are produce true
+    atleast one condition are TRUE ||
+
+NOT:-It will give reverse of number !
+*/
---
+++ Java-script/Typeconv.js
@@ -0,0 +1,54 @@
+/*Type conversion on Java-Script*/
+let score =45;
+let marks = '15';
+console.log(typeof score);
+console.log(typeof marks);
+console.log(typeof (score));//in form of method 
+
+/* Good way of writing Score */
+let Result = '35A'
+
+let valueInNumber = Number(Result);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+let someNumber = '55';
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+// output is number which is 33 but there  type is String
+console.log(typeof StringNumber);
+
+/************ Operations on Java-Script ************/
+let value ='3';
+let negValue = -value;
+console.log(negValue);
+
+console.log('1'+2);//12
+console.log(1 + '2');//12
+console.log('1'+2+2);//14 predicted but why output is 122 *To Premtive
+console.log(1+2+'2');//32
+
+//Some Tricky Conversion 
+console.log(+true);//1
+console.log(+"");//0
+
+let gameCounter = '100';
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
